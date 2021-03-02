27ac38d8e5f413d9170212ec77676e6f519116ed
mfcrespo <mafe_crespo@hotmail.com>
2021-03-01 19:47:20 -0500

    Add validations and def sanitize_text

--- koombean-net/app/models/koombean.rb
+++ koombean-net/app/models/koombean.rb
@@ -1,4 +1,29 @@
 class Koombean < ApplicationRecord
 	has_many :networks, dependent: :destroy
-  accepts_nested_attributes_for :networks, reject_if: proc { |att| att['link'].blank? }, allow_destroy: true
+  accepts_nested_attributes_for :networks, reject_if: :reject_network, allow_destroy: true
+  before_save :sanitize_text
+
+  def reject_network(attr)
+    attr['socialname'].blank? || attr['link'].blank?
+  end
+
+  def sanitize_text
+    self.firstname = firstname.titleize
+    self.lastname = lastname.titleize
+    self.city = city.titleize
+    self.country = country.titleize
+    self.email = email.downcase
+  end
+
+	validates :firstname, presence: true
+	validates :lastname, presence: true
+	validates :city, presence: true 
+	validates :country, presence: true
+	validates :phone, presence: true, length: {is:10}, 
+			    numericality: {only_integer: true}
+	validates :bio, length: {minimum:3, maximum:100}
+	validates :email, presence: true, 
+			    email: true, uniqueness: true, 
+			    length: {maximum:100}
+  
 end 
