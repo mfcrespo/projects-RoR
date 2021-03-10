66ec25b276b97c1a3de75c12ef85722b5eb1b4dd
mfcrespo <mafe_crespo@hotmail.com>
2021-03-09 20:26:03 -0500

    Add option for sign-in by username

--- twitter-clone/app/models/user.rb
+++ twitter-clone/app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :tweets, dependent: :destroy
+  before_save :sanitize_text
+
+
+       
+  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :email, presence: true, 
+             email: true, uniqueness: true, 
+             length: {maximum:100}
+
+  def sanitize_text
+    self.username = username.downcase
+    self.name = name.titleize
+    self.email = email.downcase
+  end
 end
