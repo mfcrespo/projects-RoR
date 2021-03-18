cf3ff63e1a763d8123b281d2e4644422d69ac113
mfcrespo <mafe_crespo@hotmail.com>
2021-03-18 15:50:09 -0500

    Add actual messages

--- MessageMe-Chat/db/seeds.rb
+++ MessageMe-Chat/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "jeronimo", password: "hola")
+User.create(username: "jacobo", password: "hola")
+User.create(username: "ragi", password: "hola")
+User.create(username: "emmanavarro", password: "hola")
+User.create(username: "nathsotomayor", password: "hola")
+User.create(username: "yesid", password: "hola")
