d654ea13ac8da4b4296304c68313ef8aaad7e984
mfcrespo <mafe_crespo@hotmail.com>
2021-04-03 15:39:01 -0500

    Build class method to lookup stock info

--- finance-tracker/app/models/stock.rb
+++ finance-tracker/app/models/stock.rb
@@ -1,2 +1,8 @@
 class Stock < ApplicationRecord
+
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: 'Tpk_8d66ace1a3ad4e69854312fdf53d777a',
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
+    client.price(ticker_symbol)                              
+  end  
 end
