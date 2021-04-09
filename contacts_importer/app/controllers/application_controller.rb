807ebac40311bb9acf8035bfbc4e5d68ece1ff9f
mfcrespo <mafe_crespo@hotmail.com>
2021-04-08 20:51:40 -0500

    Project relocated

--- contacts_importer/app/controllers/application_controller.rb
+++
@@ -1,6 +0,0 @@
-class ApplicationController < ActionController::Base
-
-  def after_sign_in_path_for(resource)
-    users_index_path
-  end
-end
--- contacts_importer/app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  validates :email, email: true, uniqueness: true, length: {maximum:100}
-  
-end
